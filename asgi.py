bea49ee61e9d86b66bf15b461285b9b604399da7
alerner1 <alyssaplerner@gmail.com>
2021-03-07 17:33:48 -0500

    first commit

---
+++ asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for free_the_car_backend project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.1/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'free_the_car_backend.settings')
+
+application = get_asgi_application()
