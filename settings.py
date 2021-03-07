8700c43d921fd7a5f8ef708a352c4e85f76f042f
alerner1 <alyssaplerner@gmail.com>
2021-03-07 17:55:45 -0500

    configured to use mysql

--- settings.py
+++ settings.py
@@ -76,8 +76,10 @@ WSGI_APPLICATION = 'free_the_car_backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'OPTIONS': {
+            'read_default_file': '/etc/mysql/my.cnf',
+        },
     }
 }
 
